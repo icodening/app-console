a8efbb97d506337cb13ec43362488e2fd4d487e2
icodening <cleverbn96@gmail.com>
2021-06-26 18:16:30 +0800

    把添加依赖动作提至最前，且将ClasspathInjector接口转为JDK SPI机制

--- console-agent/src/main/java/cn/icodening/console/agent/AppConsoleAgent.java
+++ console-agent/src/main/java/cn/icodening/console/agent/AppConsoleAgent.java
@@ -1,10 +1,21 @@
 package cn.icodening.console.agent;
 
 import cn.icodening.console.boot.BootServiceManager;
+import cn.icodening.console.extension.ExtensionClassLoader;
+import cn.icodening.console.injector.ClasspathInjector;
+import cn.icodening.console.injector.ClasspathRegistry;
 import cn.icodening.console.logger.Logger;
 import cn.icodening.console.logger.LoggerFactory;
+import cn.icodening.console.util.ExtensionClassLoaderHolder;
 
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ServiceLoader;
 
 /**
  * @author icodening
@@ -16,6 +27,7 @@ public class AppConsoleAgent {
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
         LOGGER.info("app console agent start");
+        addRequiredDependency();
         // 启动所有服务扩展点
         try {
             BootServiceManager.initBootServices(agentArgs);
@@ -34,5 +46,41 @@ public class AppConsoleAgent {
                 LOGGER.warn(e.getMessage(), e);
             }
         }));
+    }
+
+    private static void addRequiredDependency() {
+        ExtensionClassLoader classLoader = ExtensionClassLoaderHolder.get();
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        ServiceLoader<ClasspathInjector> load = ServiceLoader.load(ClasspathInjector.class, classLoader);
+        Iterator<ClasspathInjector> iterator = load.iterator();
+        ArrayList<ClasspathInjector> classPathConfigurers = new ArrayList<>();
+        while (iterator.hasNext()) {
+            classPathConfigurers.add(iterator.next());
+        }
+        ClasspathRegistry classPathRegistry = new ClasspathRegistry();
+        for (ClasspathInjector classPathConfigurer : classPathConfigurers) {
+            String jarPathByClass = classLoader.getJarPathByClass(classPathConfigurer.getClass().getName().replace('.', '/').concat(".class"));
+            if (jarPathByClass != null
+                    && classPathConfigurer.shouldInject()) {
+                classPathRegistry.addUrl("file:" + jarPathByClass);
+            }
+        }
+        try {
+            Method addUrlMethod = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            addUrlMethod.setAccessible(true);
+            List<String> allUrls = classPathRegistry.getAllUrl();
+            for (String u : allUrls) {
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug(u);
+                }
+                URL url = new URL(u);
+                addUrlMethod.invoke(contextClassLoader, url);
+            }
+            if (allUrls.isEmpty()) {
+                LOGGER.warn("no jar to add to the class path");
+            }
+        } catch (Exception e) {
+            LOGGER.warn(e);
+        }
     }
 }
