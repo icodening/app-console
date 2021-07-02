1e17f30d1151e3a939d28e0d6b9d5ae43a9e2e1c
icodening <cleverbn96@gmail.com>
2021-07-02 23:59:01 +0800

    调整pom配置，打包时可以将所有jar打到同一个目录下，方便debug。 调整注册时JSON序列化框架为jackson

--- console-extensions/console-boot/src/main/java/cn/icodening/console/boot/RegisterBootService.java
+++ console-extensions/console-boot/src/main/java/cn/icodening/console/boot/RegisterBootService.java
@@ -6,7 +6,6 @@ import cn.icodening.console.common.event.ServerMessageReceivedEvent;
 import cn.icodening.console.common.model.ApplicationInstance;
 import cn.icodening.console.common.model.InstanceConfigurationCache;
 import cn.icodening.console.common.model.ServerMessage;
-import cn.icodening.console.common.util.BeanMapUtil;
 import cn.icodening.console.config.ConfigurationManager;
 import cn.icodening.console.event.ConsoleEventListener;
 import cn.icodening.console.event.EventDispatcher;
@@ -15,12 +14,12 @@ import cn.icodening.console.http.Response;
 import cn.icodening.console.logger.Logger;
 import cn.icodening.console.logger.LoggerFactory;
 import cn.icodening.console.util.HttpUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -36,8 +35,16 @@ public class RegisterBootService extends BaseBootService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegisterBootService.class);
 
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void initialize(String agentArgs) {
+        this.objectMapper = new ObjectMapper();
+    }
+
     @Override
     public void start() throws AppConsoleException {
+        //FIXME magic number
         String serverAddress = ConfigurationManager.INSTANCE.get("serverAddress");
         EventDispatcher.registerOnceEvent(ApplicationInstanceStartedEvent.class, (ConsoleEventListener<ApplicationInstanceStartedEvent>) event -> {
             ApplicationInstance applicationInstance = event.getApplicationInstance();
@@ -45,36 +52,39 @@ public class RegisterBootService extends BaseBootService {
                 RegisterBootService.this.applicationInstance = applicationInstance;
                 Request post = Request.of(serverAddress + "/instance/register", "POST");
                 post.getHeaders().set("Content-Type", "application/json;charset=utf8");
-                byte[] requestBody = JSON.toJSONBytes(applicationInstance);
-                post.setBody(requestBody);
                 try {
+                    byte[] requestBody = objectMapper.writeValueAsBytes(applicationInstance);
+                    post.setBody(requestBody);
                     Response exchange = HttpUtil.exchange(post);
                     //实例注册接口响应内容为当前注册实例中所有的配置信息.
                     //其中 "data" 的数据结构为一个map， config class -> config list
                     //例子 {"data":{"cn.icodening.Config1":[],"cn.icodening.Config2":[]}, "success": true}
-                    byte[] data = exchange.getData();
-                    JSONObject json = JSON.parseObject(new String(data));
-                    String dataJsonString = json.getString("data");
-                    Map<String, List<Map<String, Object>>> stringListMap = JSON.parseObject(dataJsonString, new TypeReference<Map<String, List<Map<String, Object>>>>() {
-                    });
+                    byte[] dataBytes = exchange.getData();
+                    JsonNode jsonNode = objectMapper.readTree(dataBytes);
+                    JsonNode dataNode = jsonNode.get("data");
                     ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-                    stringListMap.forEach((key, value) -> {
+                    Iterator<Map.Entry<String, JsonNode>> fields = dataNode.fields();
+                    dataNode.fields().forEachRemaining(mapEntry -> {
                         try {
+                            String key = mapEntry.getKey();
+                            JsonNode arrayNode = mapEntry.getValue();
                             Class clz = Class.forName(key, false, contextClassLoader);
+                            List<Object> configEntitys = new ArrayList<>();
+
+                            for (int i = 0; arrayNode.has(i); i++) {
+                                JsonNode objectNode = arrayNode.get(i);
+                                Object target = objectMapper.readValue(objectNode.toString(), clz);
+                                configEntitys.add(target);
+                            }
                             ServerMessage serverMessage = new ServerMessage();
                             serverMessage.setType(clz.getName());
                             serverMessage.setSendTimestamp(System.currentTimeMillis());
-                            List<Object> objects = new ArrayList<>();
-                            for (Map<String, Object> map : value) {
-                                Object target = BeanMapUtil.mapToBean(map, clz);
-                                objects.add(target);
-                            }
-                            InstanceConfigurationCache.setConfigs(clz, objects);
-                            //FIXME setAction?
+
+                            InstanceConfigurationCache.setConfigs(clz, configEntitys);
                             ServerMessageReceivedEvent serverMessageReceivedEvent = new ServerMessageReceivedEvent(serverMessage);
                             EventDispatcher.dispatch(serverMessageReceivedEvent);
                         } catch (Exception e) {
-                            LOGGER.warn(e.getMessage(), e);
+                            LOGGER.warn("refresh instance configurations fail !!! ", e.getCause());
                         }
                     });
                     LOGGER.info("register backend success! backend address is:" + serverAddress + ". " + applicationInstance);
@@ -91,6 +101,7 @@ public class RegisterBootService extends BaseBootService {
         if (applicationInstance != null) {
             String serverAddress = ConfigurationManager.INSTANCE.get("serverAddress");
             String identity = applicationInstance.getIdentity();
+            //FIXME magic number
             Request post = Request.of(serverAddress + "/instance/deregister/" + identity, "POST");
             try {
                 HttpUtil.exchange(post);
