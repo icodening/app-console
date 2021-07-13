19bddf862718d9916497c101685e3697e5f4a199
icodening <cleverbn96@gmail.com>
2021-07-13 23:39:03 +0800

    管控服务端的websocket被关闭的同时也将关闭与应用端的websocket连接

--- console-server/src/main/java/cn/icodening/console/server/web/controller/LogWebSocketEndpoint.java
+++ console-server/src/main/java/cn/icodening/console/server/web/controller/LogWebSocketEndpoint.java
@@ -11,11 +11,15 @@ import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 import org.springframework.web.socket.server.standard.SpringConfigurator;
 
 import javax.annotation.Resource;
+import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author icodening
@@ -25,6 +29,12 @@ import javax.websocket.server.ServerEndpoint;
 @Component
 public class LogWebSocketEndpoint {
 
+    /**
+     * 应用端WebSocket Session缓存
+     * local session id => app websocket session
+     */
+    private final Map<String, WebSocketSession> webSocketSessionMap = new ConcurrentHashMap<>();
+
     @Resource
     private InstanceService instanceService;
 
@@ -33,9 +43,17 @@ public class LogWebSocketEndpoint {
         InstanceEntity instance = instanceService.findById(instanceId);
         if (instance != null) {
             WebSocketContainer webSocketContainer = session.getContainer();
+            //FIXME 暂时写死ws
             String url = "ws://" + instance.getIp() + ":" + instance.getPort() + "/log";
             StandardWebSocketClient standardWebSocketClient = new StandardWebSocketClient(webSocketContainer);
             standardWebSocketClient.doHandshake(new BinaryWebSocketHandler() {
+
+                @Override
+                public void afterConnectionEstablished(WebSocketSession webSocketSession) {
+                    String id = session.getId();
+                    webSocketSessionMap.putIfAbsent(id, webSocketSession);
+                }
+
                 @Override
                 protected void handleBinaryMessage(WebSocketSession webSocketSession, BinaryMessage message) throws Exception {
                     if (!session.isOpen()) {
@@ -47,5 +65,21 @@ public class LogWebSocketEndpoint {
                 }
             }, url);
         }
+    }
+
+    /**
+     * 浏览器与server间session关闭事件回调
+     * 关闭的同时也将与应用端的websocket断开连接
+     *
+     * @param session 当前被关闭的session
+     */
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        WebSocketSession webSocketSession = webSocketSessionMap.remove(session.getId());
+        if (webSocketSession != null) {
+            TextMessage textMessage = new TextMessage("close");
+            webSocketSession.sendMessage(textMessage);
+        }
+        session.close();
     }
 }
