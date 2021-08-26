4899e4272fb4209c7ee23481acefcc1630ebf8e4
icodening <cleverbn96@gmail.com>
2021-08-27 01:07:55 +0800

    添加agent启动流程注释

--- console-agent/src/main/java/cn/icodening/console/agent/AppConsoleAgent.java
+++ console-agent/src/main/java/cn/icodening/console/agent/AppConsoleAgent.java
@@ -19,10 +19,18 @@ public class AppConsoleAgent {
     private static final Logger LOGGER = LoggerFactory.getLogger(AppConsoleAgent.class);
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
+        //1.agent初始化，可在此阶段做字节码侵入
         agentInitializer(agentArgs, instrumentation);
+        //2.设置agent启动回调函数，并不立即启动
         AgentStartHelper.setStart(() -> startAgent(agentArgs, instrumentation));
     }
 
+    /**
+     * agent 启动前初始化
+     *
+     * @param agentArgs       启动应用时传的参数
+     * @param instrumentation 插桩工具
+     */
     private static void agentInitializer(String agentArgs, Instrumentation instrumentation) {
         try {
             ServiceLoader<AgentInitializer> load = ServiceLoader.load(AgentInitializer.class, ExtensionClassLoaderHolder.get());
@@ -34,6 +42,12 @@ public class AppConsoleAgent {
         }
     }
 
+    /**
+     * 启动agent
+     *
+     * @param agentArgs       暂时无用
+     * @param instrumentation 暂时无用
+     */
     private static void startAgent(String agentArgs, Instrumentation instrumentation) {
         LOGGER.info("app console agent start, version is v" + AppConsoleAgent.class.getPackage().getImplementationVersion());
         // 启动所有服务扩展点
